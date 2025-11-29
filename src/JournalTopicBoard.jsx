34a8403c852eae42c40398fa1d06c7fcacce31cf
ErfanBashiri95 <erfanbashiri@ymail.com>
2025-11-29 18:39:36 +0330

    final9

--- src/JournalTopicBoard.jsx
+++ src/JournalTopicBoard.jsx
@@ -431,7 +431,7 @@ export default function JournalTopicBoard({
           </p>
 
           {/* کل این بخش اسکرول می‌شود (خود پنل-scroll والدِ این div است) */}
-          <div className="flex-1 min-h-0 flex flex-col gap-2">
+          <div className="flex-1 min-h-0 max-h-[48vh] md:max-h-[52vh] scroll-area overflow-auto pr-1 flex flex-col gap-2">
             <input
               type="text"
               value={noteTitle}
