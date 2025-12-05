47b2947ca2119ba96fcac502c9ca36cd075d39c8
ErfanBashiri95 <erfanbashiri@ymail.com>
2025-12-05 14:22:57 +0330

    final10

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // برای dev روی لپ‌تاپ خودت
   server: {
     host: "0.0.0.0",
     port: 5173,
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "nil-journal.nilpapd.com",
-    ],
   },
-
-  // برای محیط deploy (npm run preview روی Railway)
   preview: {
     host: "0.0.0.0",
     port: 8080,
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-      "nil-journal.nilpapd.com",
-    ],
+    allowedHosts: ["nil-journal.nilpapd.com"],
   },
 });
