bc4339e0d4a7be3f56f69725057578013a6fc7a5
ErfanBashiri95 <erfanbashiri@ymail.com>
2025-12-05 12:58:39 +0330

    chore:allow nil-journal host in vite preview

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,23 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  preview: {
-    allowedHosts: [
-      "nil-journal.nilpapd.com",
-       
-    ],
-  },
-
+  // برای dev (لوکال)
   server: {
     host: true,
+    port: 5173,
+  },
+
+  // برای preview روی Railway
+  preview: {
+    host: true,
     port: 8080,
+    /**
+     * این خط مهم است 👇
+     * می‌تونی یا دامین خودت رو بزاری
+     * یا اگر خواستی برای همه هاست‌ها باز باشه از true استفاده کنی
+     */
+    allowedHosts: ["nil-journal.nilpapd.com"],
+    // اگر خواستی برای تمام دامین‌ها آزاد بشه:
+    // allowedHosts: true,
   },
 });
